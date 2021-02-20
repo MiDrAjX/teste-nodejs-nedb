6823e93674587cba7e01f7fb7f1bf26b3fbe22a4
MiDrAjX <jeisommarques78@gmail.com>
2021-02-20 20:23:19 -0300

    first commit

---
+++ utils/validator.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    user:(app, req, res)=>{
+        req.assert('_name', 'O nome é obrigatorio').notEmpty();
+        req.assert('_email', 'O email é obrigatorio').notEmpty().isEmail();
+
+        let errors= req.validationErrors();
+        if(errors){
+
+            app.utils.error.send(errors, req, res);
+
+            return false;
+
+        }else{
+            
+            return true;
+        }
+
+    }
+}
